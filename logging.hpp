f24389825b89f9036bab9a90ea4e92dbe97fa45f
Augustin Juricic <ajuricic@neogov.net>
2016-11-14 11:37:46 +0100

    Remove unused class.

--- logging.hpp
+++ logging.hpp
@@ -47,36 +47,3 @@ uint32_t print_solver_line(uint32_t *values, uint8_t *header, size_t fixed_nonce
 
 uint32_t print_sols(sols_t *all_sols, uint64_t *nonce, uint32_t nr_valid_sols, uint8_t *header, size_t fixed_nonce_bytes,
 	uint8_t *target, char *job_id);
-
-template<typename TimeT = std::chrono::milliseconds>
-struct measure
-{
-private:
-	std::chrono::high_resolution_clock::time_point start;
-	const char* name;
-
-public:
-	inline measure(const char* name) {
-		this->start = std::chrono::high_resolution_clock::now();
-		this->name = name;
-	}
-
-	inline TimeT stop(bool output = true) {
-		auto duration = std::chrono::duration_cast<TimeT>(std::chrono::high_resolution_clock::now() - start);
-		auto count = duration.count();
-		if (output)
-			std::cout << name << ": " << count << std::endl;
-		return duration;
-	}
-
-	template<typename F, typename ...Args>
-	static typename TimeT::rep execution(const char* name, F func, Args&&... args)
-	{
-		measure m(name);
-
-		// Now call the function with all the parameters you need.
-		func(std::forward<Args>(args)...);
-
-		return m.stop(false);
-	}
-};
